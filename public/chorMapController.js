4d548df09562956ece8a4ba518e91beb8dfe433b
marghe943 <marghe_943@yahoo.it>
2017-05-02 22:50:27 +0200

    Update chorMapController.js

--- public/chorMapController.js
+++ public/chorMapController.js
@@ -1,20 +1,20 @@
 /*  A KIBANA PLUGIN FOR CHOROPLETH MAPS
     Copyright (C) 2017  Margherita Gambini
 
-    This file is part of chorMap.
+    This file is part of choropleth_map.
 
-    chorMap is free software: you can redistribute it and/or modify
+    choropleth_map is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
 
-    chorMap is distributed in the hope that it will be useful,
+    choropleth_map is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
 
     You should have received a copy of the GNU General Public License
-    along with chorMap.  If not, see <http://www.gnu.org/licenses/>.
+    along with choropleth_map.  If not, see <http://www.gnu.org/licenses/>.
 
 /////////////////////////////////////////////////////////////////////////////////////
 
